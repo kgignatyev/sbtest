3fd0f7e20d2a4053225832c80b28b9dbc3ad07a0
kgignatyev <kgignatyev@gmail.com>
2016-02-06 12:29:31 -0800

    test

---
+++ src/test/java/kgi/tests/bamboo/sbtest/UIApiTest.java
@@ -0,0 +1,15 @@
+package kgi.tests.bamboo.sbtest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by kgignatyev on 2/6/16.
+ */
+public class UIApiTest {
+
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
